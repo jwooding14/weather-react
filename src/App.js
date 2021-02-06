4387bc1b3df76bb350dc84336c648ec21d9c24d0
jwooding14 <71294646+jwooding14@users.noreply.github.com>
2021-02-05 16:40:48 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './App.css';
+import Weather from "./Weather";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>
+         Hi!
+        </h1>
+        <Weather city="tokyo"/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
+
+
+export default function Weather(props) {
+function handleResponse(response) {
+    alert(`the weather ${response.data.name} is cool ${response.data.main.temp}`);
+}
+
+let key = "c2a0308255fedbe7dd192fcd88e7b405";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${key}&units=imperial`;
+axios.get(apiUrl).then(handleResponse);
+return (  
+<Loader
+    type="Puff"
+    color="#00BFFF"
+    height={100}
+    width={100}
+    timeout={3000}
+  />
+  );
+}
